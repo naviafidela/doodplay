648d56e25b5ef047a65c55b5f2396946ae7df4c4
naviafidela <naviafidela@gmail.com>
2025-11-11 16:44:27 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+# bot.py
+from pyrogram import Client
+import asyncio
+
+# Ganti dengan kredensial bot Anda
+API_ID = 21404483                # Dapatkan dari https://my.telegram.org
+API_HASH = "c60cf837b400cd918163820b314ef8d0" # Dapatkan dari https://my.telegram.org
+BOT_TOKEN = "8489286033:AAEp8BDY2IiAFb4YmqXCKCIdxGcmV70iHQo"  # Dapatkan dari @BotFather
+
+# Inisialisasi Client Pyrogram
+app = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
+
+async def main():
+    print("🤖 Bot sedang berjalan...")
+    await app.start()
+    await idle()
+    await app.stop()
+    print("❌ Bot dihentikan.")
+
+if __name__ == "__main__":
+    from pyrogram import idle
+    asyncio.run(main())
