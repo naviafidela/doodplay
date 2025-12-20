ea8a871a3be999463f3d7854fa639527c8c2b385
naviafidela <naviafidela@gmail.com>
2025-12-20 17:31:10 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -71,7 +71,7 @@ async def start_command(client, message):
                                 await message.reply_photo(
                                     photo=poster,
                                     caption=(
-                                        f"🎥 {caption_title}\n\n\n\n@JapanBestChannel"
+                                        f"🎥 {caption_title}\n\n@JapanBestChannel"
                                     ),
                                     parse_mode=ParseMode.HTML,
                                     reply_markup=buttons,
